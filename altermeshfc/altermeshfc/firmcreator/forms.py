5afd804c64fdbaead61f0aa481de31c1bf3069f1
Santiago Piccinini (SAn) <piccinini.santiago@gmail.com>
2012-11-03 15:05:48 -0300

    Added migrations

---
+++ altermeshfc/altermeshfc/firmcreator/forms.py
@@ -0,0 +1,183 @@
+# -*- coding: utf-8 -*-
+import os
+import datetime
+
+from django import forms
+from django.forms.widgets import TextInput, Textarea, FileInput, DateInput
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
+from django.contrib.admin.widgets import FilteredSelectMultiple
+from django.forms.formsets import formset_factory
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Div, Submit, HTML, Button, Row, Field, Fieldset
+from crispy_forms.bootstrap import AppendedText, PrependedText, FormActions
+
+from models import IncludePackages, IncludeFiles, FwProfile, Network
+
+# We may add a description of each package, in the form ("pkgname", "description")
+SUGESTED_PACKAGES = ["iperf", "uhttpd", "mini-snmpd"]
+
+class BaseForm(forms.Form):
+    helper = FormHelper()
+    helper.form_tag = False
+    helper.form_class = 'form-horizontal'
+
+class IncludePackagesForm(BaseForm):
+
+    optional_packages = forms.MultipleChoiceField(
+        label = _("Optional packages"),
+        choices = [(p, p) for p in SUGESTED_PACKAGES],
+        widget = forms.CheckboxSelectMultiple,
+        help_text = _("This packages are not needed but you may enjoy from them."),
+        required = False,
+    )
+
+    include_exclude = forms.CharField(
+        label = _("Extra include/exclude packages"),
+        help_text = _("list of packages to include and exclude in the following form to include foo and baz and exclude bar: <tt>foo -bar baz</tt>"),
+        required = False,
+    )
+
+    @classmethod
+    def from_instance(self, instance):
+        return IncludePackagesForm({"include_exclude":instance.to_str()})
+
+    @classmethod
+    def from_str(self, string):
+        ip = IncludePackages.from_str(string)
+        optional_packages = []
+        for package in ip.include[:]:
+            if package in SUGESTED_PACKAGES:
+                optional_packages.append(package)
+                ip.include.remove(package)
+        return IncludePackagesForm({"include_exclude":ip.to_str(), "optional_packages": optional_packages})
+
+    def to_str(self):
+        optional_packages = self.cleaned_data.get("optional_packages")
+        include_exclude = self.cleaned_data.get("include_exclude")
+        ip = IncludePackages.from_str(include_exclude)
+        ip.include = set(ip.include + optional_packages)
+        return ip.to_str()
+
+class IncludeFileForm(BaseForm):
+
+    name = forms.CharField(
+        help_text="eg: /etc/conf/batmesh"
+    )
+
+    content = forms.CharField(
+        widget = forms.Textarea(),
+    )
+
+    helper = FormHelper()
+    helper.form_tag = False
+    helper.form_style = 'inline'
+    helper.form_class = 'form-horizontal'
+    helper.layout = Layout(
+            Fieldset(_("File"),
+                Field('name', css_class="input-xxlarge"),
+                Field('content', css_class="input-xxlarge pre-scrollable"),
+            )
+        )
+
+    @classmethod
+    def from_instance(self, instance):
+        return IncludePackagesForm({"include_exclude":instance.to_str()})
+
+class NetworkForm(forms.ModelForm):
+
+    class Meta:
+        model = Network
+        exclude = (
+            'slug', 'user',
+        )
+
+class FwProfileSimpleForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user')
+        super(FwProfileSimpleForm, self).__init__(*args, **kwargs)
+        instance = kwargs.get('instance', None)
+        self.fields['network'] = forms.ModelChoiceField(queryset=Network.objects.filter(user=user))
+
+    helper = FormHelper()
+    helper.form_tag = False
+    helper.form_class = 'form-horizontal'
+
+    class Meta:
+        model = FwProfile
+        exclude = (
+            'cretion_date', 'path', 'include_packages', 'include_files',
+        )
+
+class FwProfileForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user')
+        super(FwProfileForm, self).__init__(*args, **kwargs)
+        instance = kwargs.get('instance', None)
+        self.fields['network'] = forms.ModelChoiceField(queryset=Network.objects.filter(user=user))
+
+
+    def save(self, user, *args, **kwargs):
+        kwargs['commit'] = False
+        obj = super(FwProfileForm, self).save(*args, **kwargs)
+        obj.user = user
+        obj.save()
+        self.save_m2m()
+        return obj
+
+    helper = FormHelper()
+    helper.form_tag = False
+    helper.form_class = 'form-horizontal'
+
+    class Meta:
+        model = FwProfile
+        exclude = (
+            'cretion_date', 'path', 'include_files', 'include_packages'
+        )
+
+IncludeFilesFormset = formset_factory(IncludeFileForm, extra=0)
+
+COMMON_DEVICES = (
+    ('TLMR3020', 'TP-LINK TL-MR3020'),
+    ('TLMR3220', 'TP-LINK TL-MR3220'),
+    ('TLMR3420', 'TP-LINK TL-MR3420'),
+    ('UBNT', 'Ubiquiti Bullet M'),
+    ('NONEatherosDefault', 'Ubiquiti Bullet 2, NanoStation 2, NanoLoco 2'),
+)
+
+def get_supported_devices():
+    return {"ar71xx": ["TLMR3020", ]}
+
+class CookFirmwareForm(forms.Form):
+    common_devices = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,
+                                               required=False,
+                                               choices=COMMON_DEVICES,
+                                               label=_("Common devices"))
+    other_devices = forms.CharField(required=False, label=_("Other devices"),
+                                    widget=forms.Textarea,
+                                    help_text=_("List of PROFILE devices separated by a space or EOL. Eg: <code>TLMR3420 UBNT TLWA701</code>"),
+                                    )
+    openwrt_revision = forms.SlugField(required=True, initial="stable")
+
+    def get_devices(self):
+        return self.cleaned_data["common_devices"] + self.cleaned_data["other_devices"].split()
+
+    def clean(self):
+        cleaned_data = super(CookFirmwareForm, self).clean()
+        common_devices = cleaned_data.get("common_devices")
+        other_devices = cleaned_data.get("other_devices")
+
+        if not (common_devices or other_devices):
+            raise forms.ValidationError(_("You must select at least one device."))
+
+        devices = self.get_devices()
+        if not all(map(lambda x: x.isalnum(), devices)):
+            raise forms.ValidationError(_("Devices must contains only alphanumeric characters."))
+
+        return cleaned_data
+
+
---
+++ altermeshfc/altermeshfc/firmcreator/tests.py
@@ -0,0 +1,130 @@
+import os
+from os import path
+import shutil
+import StringIO
+import tempfile
+import subprocess
+
+from django.test import TestCase
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+
+from models import IncludePackages, IncludeFiles, FwJob, FwProfile, Network
+from forms import IncludePackagesForm, CookFirmwareForm
+
+TEST_DATA_PATH = path.join(path.dirname(__file__), "test_data")
+PROFILES_PATH =  path.abspath(path.join(TEST_DATA_PATH, "profiles"))
+TEST_PROFILE_PATH = path.join(PROFILES_PATH, "test.org.ar")
+
+class ViewsTest(TestCase):
+    def test_index(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+class IncludePackagesTest(TestCase):
+    def test_read_from_disk(self):
+        ip = IncludePackages.load(open(path.join(TEST_PROFILE_PATH, "include_packages")))
+        self.assertEqual(ip.include, ["kmod-batman-adv", "kmod-ipv6", "dnsmasq-dhcpv6",
+                                      "ip6tables", "kmod-ath9k-htc", "safe-reboot", "iperf"])
+        self.assertEqual(ip.exclude, ["dnsmasq", "qos-scripts"])
+
+    def test_write_to_disk(self):
+        ip = IncludePackages(include=["iperf", "safe-reboot"],
+                             exclude=["dnsmasq", "qos-scripts"])
+        output = StringIO.StringIO()
+        ip.dump(output)
+        self.assertEqual(output.getvalue(), "iperf safe-reboot -dnsmasq -qos-scripts")
+
+    def test_form(self):
+        ip = IncludePackages(include=["iperf", "safe-reboot"],
+                             exclude=["dnsmasq", "qos-scripts"])
+        form = IncludePackagesForm.from_instance(ip)
+        self.assertTrue(form.is_valid())
+
+    def test_string(self):
+        ip = IncludePackages.from_str("")
+        self.assertEqual(ip.to_str(), "")
+
+
+class IncludeFilesTest(TestCase):
+
+    def read_file(self, filename):
+        return open(path.join(TEST_PROFILE_PATH, "include_files", filename)).read()
+
+    def read_test_files(self):
+        files = {}
+        for filename in ["/etc/profile", "/etc/config/batman-adv", "/etc/config/batmesh",
+                         "/etc/uci-defaults/add-opkg-repo-altermundi",
+                         "/etc/uci-defaults/set-timezone-art3"]:
+            files[filename] = self.read_file(filename[1:])
+        return files
+
+    def test_read_from_disk(self):
+        inc_files = IncludeFiles.load(path.join(TEST_PROFILE_PATH, "include_files"))
+
+        self.assertEqual(inc_files.files, self.read_test_files())
+
+    def test_write_to_disk(self):
+        inc_files = IncludeFiles(files=self.read_test_files())
+        dest_dir = tempfile.mkdtemp()
+        inc_files.dump(dest_dir)
+        diff = subprocess.check_output(["diff", "-r", dest_dir, path.join(TEST_PROFILE_PATH, "include_files")], stderr=subprocess.STDOUT)
+        self.assertEqual(diff, "")
+        shutil.rmtree(dest_dir) # cleaning up
+
+class JobsTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user("ninja", "e@a.com", "password")
+        self.network = Network.objects.create(name="quintanalibre.org.ar", user=self.user)
+        self.profile = FwProfile.objects.create(network=self.network)
+        self.client.login(username="ninja", password="password")
+        self.cook_url = reverse('cook', kwargs={'slug': self.profile.slug})
+
+    def _test_process_some_jobs(self):
+        fwjob = FwJob.objects.create(profile=self.profile, user=self.user)
+        fwjob = FwJob.objects.create(profile=self.profile, user=self.user)
+
+        import time
+        self.assertEqual(len(FwJob.started.all()), 0)
+        self.assertEqual(len(FwJob.waiting.all()), 2)
+        FwJob.process_jobs()
+        time.sleep(0.5)
+        self.assertEqual(len(FwJob.started.all()), 1)
+        self.assertEqual(len(FwJob.waiting.all()), 1)
+        FwJob.process_jobs()
+        time.sleep(0.5)
+
+        self.assertEqual(len(FwJob.started.all()), 1)
+        self.assertEqual(len(FwJob.waiting.all()), 0)
+        FwJob.process_jobs()
+        time.sleep(0.5)
+        self.assertEqual(len(FwJob.started.all()), 0)
+        self.assertEqual(len(FwJob.waiting.all()), 0)
+
+    def _test_cook(self):
+        response = self.client.get(self.cook_url)
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.post(self.cook_url, {"other_devices":"TLMR3020", "openwrt_revision": "stable"})
+        self.assertEqual(response.status_code, 302)
+
+        self.assertEqual(len(FwJob.started.all()), 0)
+        self.assertEqual(len(FwJob.waiting.all()), 1)
+        FwJob.process_jobs()
+        self.assertEqual(len(FwJob.started.all()), 1)
+        self.assertEqual(len(FwJob.waiting.all()), 0)
+
+    def test_empty_cook(self):
+        response = self.client.post(self.cook_url, {})
+        self.assertContains(response, "You must select at least one device.")
+
+    def test_inyect_cook(self):
+        response = self.client.post(self.cook_url, {"other_devices": "MR320;comando"})
+        self.assertContains(response, "Devices must contains only alphanumeric characters.")
+
+    def test_make_commands(self):
+        from models import make_commands
+        commands = make_commands("quintanalibre.org.ar", ["TLMR3220", "NONEatherosDefault"], "33333")
+        self.assertTrue("33333 ar71xx quintanalibre.org.ar TLMR3220" in commands[0])
+        self.assertTrue("33333 atheros quintanalibre.org.ar Default" in commands[1])
