4fca1df02911bde7c83766cf9e7035f0c310726c
nicoechaniz <nicoechaniz@altermundi.net>
2013-12-12 19:23:04 -0300

    serve static files when settings.DEBUG is True

--- altermeshfc/altermeshfc/urls.py
+++ altermeshfc/altermeshfc/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 from altermeshfc.firmcreator.views import NetworkCreateView, NetworkDetailView, NetworkListView, \
                                            NetworkUpdateView, NetworkDeleteView, FwProfileDetailView, \
@@ -47,3 +48,8 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
